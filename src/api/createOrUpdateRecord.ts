bee304b86b50c6897f65bd1ee72ab83fd706bdcf
owpk <vzvz4@mail.ru>
2025-10-13 23:46:02 +0800

    HOTFIX-API: fix base path

--- src/api/createOrUpdateRecord.ts
+++ src/api/createOrUpdateRecord.ts
@@ -5,7 +5,7 @@ import { http } from '@/services';
 
 export const createOrUpdateRecord = flow(
   (record: typeof RecordWithOptionalId.Type): [string, RequestInit] => [
-    'api/v1/record',
+    '/api/v1/record',
     {
       method: 'POST',
       body: JSON.stringify(Schema.encodeSync(RecordWithOptionalId)(record)),
