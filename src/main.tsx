7bac5b67fe3fd6c7ca50ea7f938be8fbb1930ce8
gamabuntat <schoshoev@yandex.ru>
2025-12-28 17:13:47 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -89,47 +89,15 @@ swipeBehavior.mount();
 swipeBehavior.disableVertical();
 
 const { top, left, right, bottom } = viewport.safeAreaInsets();
-document.documentElement.style.setProperty(
-  '--tg-safe-area-inset-left',
-  `${left}px`,
-);
-document.documentElement.style.setProperty(
-  '--tg-safe-area-inset-right',
-  `${right}px`,
-);
-document.documentElement.style.setProperty(
-  '--tg-safe-area-inset-bottom',
-  `${bottom}px`,
-);
-document.documentElement.style.setProperty(
-  '--tg-safe-area-inset-top',
-  `${top}px`,
-);
 
 viewport.bindCssVars();
 if (import.meta.env.PROD) {
   alert(JSON.stringify(viewport.contentSafeAreaInsets()));
   alert(JSON.stringify(viewport.safeAreaInsets()));
-  alert(JSON.stringify(document.documentElement.style.getPropertyValue('--tg-viewport-safe-area-inset-bottom')))
+  alert(JSON.stringify(document.documentElement.style.getPropertyValue('--tg-safe-area-inset-bottom')))
 }
 
 on('safe_area_changed', ({ top, left, right, bottom }) => {
-  document.documentElement.style.setProperty(
-    '--tg-safe-area-inset-left',
-    `${left}px`,
-  );
-  document.documentElement.style.setProperty(
-    '--tg-safe-area-inset-right',
-    `${right}px`,
-  );
-  document.documentElement.style.setProperty(
-    '--tg-safe-area-inset-bottom',
-    `${bottom}px`,
-  );
-  document.documentElement.style.setProperty(
-    '--tg-safe-area-inset-top',
-    `${top}px`,
-  );
 });
 
 createRoot(document.getElementById('root')!).render(
